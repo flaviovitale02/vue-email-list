d22cc4d2a947b00688e154ee636f9e724c90a303
flaviovitale02 <flaviovitale73@gmail.com>
2023-06-15 18:19:16 +0200

    prima commit

---
+++ script.js
@@ -0,0 +1,26 @@
+
+
+const app = Vue.createApp({
+    data() {
+        return {
+            EmailList: [
+
+            ]
+
+        }
+    },
+    methods: {
+        getRandomEmail() {
+            for (i = 0; i < 10; i++) {
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                    .then((result) => this.EmailList.push(result.data.response))
+            }
+
+        },
+    },
+    created() {
+        this.getRandomEmail();
+    },
+})
+
+app.mount('.root')
